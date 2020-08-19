cea70b2217a8a3ec8bfada17be227a2090ba2e1d
Cameron Kingsley <cameron.kingsley13@gmail.com>
2020-08-19 06:56:13 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -20,7 +20,7 @@ class Triangle
 
     if( @sideA == @sideB && @sideB == @sideC)
       :equilateral
-    elsif (@sideA == @sideB && @sideB != @sideC)
+    elsif (@sideA == @sideB && @sideB != @sideC || @sideA != @sideB && @sideB == @sideC )
       :isosceles
     elsif (@sideA != @sideB && @sideB != @sideC && @sideA != @sideC)
       :scalene
